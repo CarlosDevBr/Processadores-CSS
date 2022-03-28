889b3b5eafff5b25c90380b4f6742b2e8894ea9d
CarlosDevBr <cahzerohenrique@gmail.com>
2022-03-28 16:14:56 -0300

    Fixed

---
+++ less/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.render('../views/less')
+});
+
+app.get('/less', function(req, res){
+    res.render('../views/less')
+});
+
+app.get('/historia', function(req, res){
+    res.render('../views/historia')
+});
+
+app.listen(3001, function() {
+    console.log('Executando na porta 3001');
+});
