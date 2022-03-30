b880b135e27c8cdf039024b3295bde3b26e75606
CarlosDevBr <cahzerohenrique@gmail.com>
2022-03-30 12:56:34 -0300

    6.D: Created tag const

--- postCss/meu-app/src/App.js
+++ postCss/meu-app/src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Titulo = styled.h1`
+color: #31e636;
+`;
+
+const SubTitulo = styled.h2`
+color: green;
+font-size: 30px;
+text-align: left;
+`;
+
+const Artigo = styled.section`
+color: #9393e2;
+`;
+
+const Titulo2 = styled.h1`
+color: blue;
+font-size: 25px;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Titulo>Meu site</Titulo>
+        <SubTitulo>Subtitulo</SubTitulo>
+        <Artigo>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque minus recusandae qui eligendi accusamus dolor. Et error ab maxime quaerat! Ut nemo doloremque quisquam ipsam blanditiis nam, eius delectus fugit!</Artigo>
+        <Titulo2>Segunda Parte</Titulo2>
+        <Artigo>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque minus recusandae qui eligendi accusamus dolor. Et error ab maxime quaerat! Ut nemo doloremque quisquam ipsam blanditiis nam, eius delectus fugit!</Artigo>
       </header>
     </div>
   );
